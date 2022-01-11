3a708b40df4cb1a7b4d17a72b412a9aafabe4761
YunKukPark <a01043340999@gmail.com>
2022-01-11 13:23:41 +0900

    ADD: Login page UI 완료 - 로그인페이지 UI 완료 - 소셜로그인 8과정 중 2과정까지 완료 - 회원가입페이지가 필요없어짐에 따라 제거 - style Mixin 제작 - 리뷰 내용 반영

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,96 @@
 import React from 'react';
+import { KAKAO_AUTH_URL } from 'socialLogin/KakaoLogin';
+import styled, { keyframes } from 'styled-components';
+import { flexBox } from 'styles/mixins';
+
+const { Kakao } = window;
 
 function Login(props) {
-  return <div>여긴 로그인이다.</div>;
+  return (
+    <Container>
+      <ContentBox>
+        <ContentRow as="header">
+          <ContentAnimationBox>
+            <Emoji>👋</Emoji>
+          </ContentAnimationBox>
+          <Title>반갑습니다!</Title>
+          <Greeting>인테리어의 모든 것, 동기생활관</Greeting>
+          <LoginButton>
+            <img
+              src="/images/kakao_login_large_wide.png"
+              alt="카카오톡로그인"
+            />
+          </LoginButton>
+        </ContentRow>
+      </ContentBox>
+    </Container>
+  );
 }
+
+const Container = styled.section`
+  ${flexBox()}
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.background};
+`;
+
+const ContentRow = styled.div`
+  text-align: center;
+`;
+
+const sayHelloAnimation = keyframes`
+  from {
+    transform: rotate(-20deg);
+  }to{
+    transform: rotate(10deg);
+  }
+`;
+
+const ContentAnimationBox = styled(ContentRow)`
+  animation: ${sayHelloAnimation} 1s linear infinite alternate;
+`;
+
+const Emoji = styled.p`
+  font-size: 60px;
+  margin-bottom: 40px;
+`;
+
+const Title = styled.h2`
+  font-size: 40px;
+  font-weight: 700;
+  margin-bottom: 20px;
+`;
+
+const Greeting = styled.p`
+  text-align: center;
+  margin-bottom: 50px;
+`;
+
+const ContentBox = styled.div`
+  ${flexBox()}
+  width: 500px;
+  height: 500px;
+  background-color: ${({ theme }) => theme.white};
+  padding: 60px;
+`;
+
+const LoginButton = styled.a.attrs({ href: KAKAO_AUTH_URL })`
+  display: block;
+  width: 100%;
+  margin: 0 auto;
+  border: 0;
+  background-color: ${({ theme }) => theme.white};
+
+  img {
+    display: block;
+    width: 100%;
+    transition: box-shadow 0.2s;
+    border-radius: 10px;
+
+    &:hover {
+      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.18);
+    }
+  }
+`;
 
 export default Login;
