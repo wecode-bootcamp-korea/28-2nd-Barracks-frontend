6b5ed7ad4c4af3c0ca34cabfa255f232b527e286
한상일 <mint@hansang-il-ui-MacBookAir.local>
2022-01-12 13:54:17 +0900

    Add: 메인 페이지 레이아웃 작업 - 메인 페이지 레이아웃 - 메인 페이지 슬라이더, 카드 컴포넌트 작성 - css 피드백 사항 반영하여 코드수정

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import Router from './Router';
+
 import GlobalStyle from 'styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { color, mixins } from 'styles/theme';
+
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
-import Router from './Router';
 
 ReactDOM.render(
   <>
