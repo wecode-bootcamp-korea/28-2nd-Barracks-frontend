4b9e4ff755e00aff888f128d152a8acd75de4a2b
YunKukPark <a01043340999@gmail.com>
2022-01-13 14:47:30 +0900

    Modify: 소셜 로그인 토큰 받아오기 완료 - 기존 인가코드만 받아왔던 버그 수정 - 토큰까지 받아오게 수정

--- src/Router.js
+++ src/Router.js
@@ -4,12 +4,14 @@ import Login from 'pages/Login/Login';
 import Main from 'pages/Main/Main';
 import Detail from 'pages/Detail/Detail';
 import NewPost from 'pages/NewPost/NewPost';
+import KakaoRequest from 'socialLogin/KakaoRequest';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/users/login" element={<Login />} />
+        <Route path="/oauth" element={<KakaoRequest />} />
         <Route path="/" element={<Main />} />
         <Route path="/contents/:id" element={<Detail />} />
         <Route path="/contents/new" element={<NewPost />} />
--- src/socialLogin/KakaoLogin.js
+++ src/socialLogin/KakaoLogin.js
@@ -1,4 +1,6 @@
+const HOST_URL = 'https://kauth.kakao.com';
 const CLIENT_ID = process.env.REACT_APP_KAKAO_REST_API_KEY;
 const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT;
 
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_GET_AUTH_URL = `${HOST_URL}/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+export const KAKAO_GET_TOKEN_URL = `${HOST_URL}/oauth/token`;
